0b5dc05affea833100211a10dc9d22ae074b40f7
mohamim360 <mohamim360@gmail.com>
2023-06-20 17:27:59 +0600

    react router added

---
+++ src/Layout/Main.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import NavBer from '../NavBer/NavBer';
+
+const Main = () => {
+  return (
+    <div>
+      <NavBer></NavBer>
+    </div>
+  );
+};
+
+export default Main;
